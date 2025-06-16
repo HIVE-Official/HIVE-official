fcde15c8796aa7adea5d1b63bcb6a5a4b25c9a38
rhinehart514 <65137255+rhinehart514@users.noreply.github.com>
2025-06-16 18:51:10 -0400

    chore: remove storybook static files

--- packages/ui/storybook-static/assets/grid.stories-BhKEz56H.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./jsx-runtime-CDt2p4po.js";import{G as c,B as r}from"./grid-BjptKGNo.js";import"./index-GiUgBvb1.js";import"./utils-CytzSlOG.js";import"./index-BwobEAja.js";const i={title:"Components/Grid",component:c,tags:["autodocs"]},n=e.jsxs(e.Fragment,{children:[e.jsx(r,{className:"p-4 bg-bg-card rounded-lg",children:"Item 1"}),e.jsx(r,{className:"p-4 bg-bg-card rounded-lg",children:"Item 2"}),e.jsx(r,{className:"p-4 bg-bg-card rounded-lg",children:"Item 3"}),e.jsx(r,{className:"p-4 bg-bg-card rounded-lg",children:"Item 4"}),e.jsx(r,{className:"p-4 bg-bg-card rounded-lg",children:"Item 5"}),e.jsx(r,{className:"p-4 bg-bg-card rounded-lg",children:"Item 6"})]}),s={args:{cols:3,gap:4,children:n}};var a,o,t;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    cols: 3,
-    gap: 4,
-    children: content
-  }
-}`,...(t=(o=s.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const u=["Default"];export{s as Default,u as __namedExportsOrder,i as default};
